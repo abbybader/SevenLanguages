3ae04bcd0185bbace7fb8b37bf5e90813bdf38c2
abbybader <abby.bader@spectraseis.com>
2013-04-14 15:52:11 -0600

    Create day1-exercises.rb

---
+++ day1-exercises.rb
@@ -0,0 +1,20 @@
+puts 'print "Hello, Ruby"'
+string = "Hello, Ruby"
+puts string
+
+
+puts
+puts
+
+puts 'find index of "Ruby"'
+index = string.index("Ruby")
+puts "Index is #{index}"
+##  also -  "Hello, Ruby".index("Ruby")
+
+puts
+puts
+
+puts "print your name 10 times"
+10.times {puts "Abby Bader"}
+
+
